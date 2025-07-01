1317386b443ba5c754951a37d52c2b47ab1934d9
amandasclonts <amandaclonts@hotmail.com>
2025-07-01 08:11:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ if not os.path.exists(excel_file):
 st.title("Tool Checkout Log")
 
 name = st.text_input("Your Name")
-item = st.selectbox("Select Item", ["Hammer", "Drill", "Screwdriver", "Wrench", "Tape Measure"])  # Customize list
+item = st.selectbox("Select Item", ["DB-1/8"", "DB-30", "DB-3/16"", "DB-1/4"", "DB-3/16" SDS", "DB-1/4" SDS", "CSB-7 1/4"", "CSB-6 1/2"", CSB-5 3/8"", "CSB-7 1/4" AL", 
+"CSB-6 1/2"AL", "CW-4 1/2"", "CW-5"", "RZBLD", "BSB-44 7/8"", "RSB-6"", "RSB-12"", "JSB-24T", "JSB-TS", "ACM-RB", "BTH-3"", "BTH-6"", "PBT 2R", "PBT 3", 
+"SBT 2", "SBT 3", "TBT-T25", "DCB", "ND-1/4"x6"", "ND-5/16" x6"", "ND-3/8"x6"", "STLN", "RK-Jw/s", "RK-PT", "STPL-T-50", "8-1" ST", "8-3/4" ST"])  # Customize list
 quantity = st.number_input("Quantity", min_value=1, step=1)
 
 if st.button("Submit"):
